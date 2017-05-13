bc2c96676d72a6a2d3d812f41558cf660b26c3e1
koretskyhub <koretsky97@gmail.com>
2017-05-14 02:15:27 +0300

    Update server.install.sh

--- server.install.sh
+++ server.install.sh
@@ -9,7 +9,7 @@ sudo apt-get install apache2 php7.0 libapache2-mod-php7.0
 sudo cp /home/BALinux/000-default.conf /etc/apache2/sites-available/
 sudo cp /home/BALinux/ports.conf /etc/apache2/
 sudo cp /home/BALinux/default /etc/nginx/sites-available/
-sudo cp /home/BALinux/sysinfo.php /var/www/http/
-sudo rm -rf /var/www/http/index.*
+sudo cp /home/BALinux/sysinfo.php /var/www/html/
+sudo rm -rf /var/www/html/index.*
 service nginx reload
 service apache2 reload 
