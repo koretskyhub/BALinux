846f69c26a423f3db0a86c9b7d0ddb06408a35aa
koretskyhub <koretsky97@gmail.com>
2017-05-14 02:16:49 +0300

    Update server.install.sh

--- server.install.sh
+++ server.install.sh
@@ -3,9 +3,6 @@ sudo apt-get update
 sudo apt-get install lynx 
 sudo apt-get install nginx
 sudo apt-get install apache2 php7.0 libapache2-mod-php7.0 
-#sudo chmod -R 777 /etc/apache2/ports.conf 
-#sudo chmod -R 777 /etc/apache2/sites-available/000-default.conf 
-#sudo chmod -R 777 /etc/nginx/sites-available/default
 sudo cp /home/BALinux/000-default.conf /etc/apache2/sites-available/
 sudo cp /home/BALinux/ports.conf /etc/apache2/
 sudo cp /home/BALinux/default /etc/nginx/sites-available/
