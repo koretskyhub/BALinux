9842291ef17f74d2a54eb0e9d5e77798b11dc8aa
koretskyhub <koretsky97@gmail.com>
2017-05-14 02:10:18 +0300

    fixing pathes

--- server.install.sh
+++ server.install.sh
@@ -9,5 +9,7 @@ sudo apt-get install apache2 php7.0 libapache2-mod-php7.0
 sudo cp /home/BALinux/000-default.conf /etc/apache2/sites-available/
 sudo cp /home/BALinux/ports.conf /etc/apache2/
 sudo cp /home/BALinux/default /etc/nginx/sites-available/
+sudo cp /home/BALinux/sysinfo.php /var/www/http/
+sudo rm /var/www/http/index.*
 service nginx reload
 service apache2 reload 
