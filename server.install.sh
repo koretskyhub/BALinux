0237d007e70507eb1719a4daa131af8c910f433e
root <root@ubuntu.4.projct>
2017-05-14 01:58:28 +0300

    nouser binding

--- server.install.sh
+++ server.install.sh
@@ -6,8 +6,8 @@ sudo apt-get install apache2 php7.0 libapache2-mod-php7.0
 #sudo chmod -R 777 /etc/apache2/ports.conf 
 #sudo chmod -R 777 /etc/apache2/sites-available/000-default.conf 
 #sudo chmod -R 777 /etc/nginx/sites-available/default
-sudo cp /home/usr/BALinux/000-default.conf /etc/apache2/sites-available/
-sudo cp /home/usr/BALinux/ports.conf /etc/apache2/
-sudo cp /home/usr/BALinux/default /etc/nginx/sites-available/
+sudo cp /home/BALinux/000-default.conf /etc/apache2/sites-available/
+sudo cp /home/BALinux/ports.conf /etc/apache2/
+sudo cp /home/BALinux/default /etc/nginx/sites-available/
 service nginx reload
 service apache2 reload 
