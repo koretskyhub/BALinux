5acd7f334e8974190fb6ad620d884cd5882df064
koretskyhub <koretsky97@gmail.com>
2017-05-14 16:14:50 +0300

    First Release

--- server.install.sh
+++ server.install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get install lynx 
+sudo apt-get install sysstat
 sudo apt-get install nginx
 sudo apt-get install apache2 php7.0 libapache2-mod-php7.0 
 sudo cp /home/BALinux/000-default.conf /etc/apache2/sites-available/
--- sysinfo.php
+++ sysinfo.php
@@ -1,4 +1,4 @@
-<?php
+<?
 $lavg1 = exec('cut -f1 -d" " /proc/loadavg');
 $lavg5 = exec('cut -f2 -d" " /proc/loadavg');
 $lavg15 = exec('cut -f3 -d" " /proc/loadavg');
@@ -18,12 +18,50 @@ function coloring($lavg)
   <meta charset="utf-8">
   <title>SYSTEM INFO</title>
   <style>
+  * {
+        font-family: 'Courier New';
+    }
+  h1 {
+        text-align: center;
+     }
   </style>
  </head>
  <body>
+<H1 class ="h";>Сокет клиента</H1>
+<?php echo $_SERVER['REMOTE_ADDR'];
+echo "<br>";?>
+<hr>
+<H1>Сокет прокси-NGINX</H1>
+<?php echo $_SERVER['HTTP_X_FORWARDED_FOR'];
+//echo ":";
+//echo $_SERVER['REMOTE_PORT'];
+//echo "<br>";
+//echo $_SERVER['NGINX_VERSION'];
+?>
+  <hr>
+  <H1>Load Average</H1>
   <table>
-  <tr><td>LoadAverade 1 minute</td> <td>LoadAverade 5 minute</td> <td>LoadAverade 15 minute</td></tr>
-  <tr><td><?php echo coloring($lavg1);?></td> <td><?php echo coloring($lavg5);?></td> <td><?php echo coloring($lavg15); ?></td></tr>
+  <tr><td>LA for 1 minute</td><td>LA for 5 minute's</td><td>LA for 15 minute's</td></tr>
+  <tr><td><?php echo coloring($lavg1);?></td><td><?php echo coloring($lavg5);?></td><td><?php echo coloring($lavg15); ?></td></tr>
   </table>
- </body>
+  <br>
+  <hr>
+  <H1>Загрузка Дисков</H1>
+  <pre><?php system('iostat -dx'); ?></pre>
+  <hr>
+  <H1>Загрузка Сети</H1>
+  <pre><?php system('cat /proc/net/dev')  ?></pre>
+  <hr>
+  <H1>Top Talkers</H1>
+  <pre><?php system('tcpdump -l -c10 -i any');  ?></pre>
+  <hr>
+  <H1>Сетевые соединения</H1>
+  <pre><?php system('netstat -tlpn');  ?></pre>
+  <hr>
+  <H1>Загрузка CPU</H1>
+  <pre><?php system('mpstat'); ?></pre>
+  <hr>
+  <H1>Информация о дисках</H1>
+  <pre><?php echo system('df -h'); ?></pre>
+</body>
 </html>
